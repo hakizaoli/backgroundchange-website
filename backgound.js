89c57f3ad5c9f08b47f78d441eccc2459d9cd3bc
hakizaoli <148442654+hakizaoli@users.noreply.github.com>
2024-05-16 22:11:20 +0200

    Add files via upload

---
+++ backgound.js
@@ -0,0 +1,9 @@
+var index = 0;
+
+function changeColors(){
+  var colors = ["red", "blue", "orange","green", "yellow", "purple"];
+  document.getElementsByTagName("body")[0].style.background = colors[index++];
+
+  if(index > colors.length - 1)
+  index = 0;
+}
